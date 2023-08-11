ff6babbc1cc249f2522637f093e52a687d25238f
Jakob Spahn <jakob@craalse.de>
2023-08-11 23:46:05 +0200

    much smaller model

--- test/models/test_bert.py
+++ test/models/test_bert.py
@@ -27,8 +27,8 @@ class TestBert(unittest.TestCase):
     from transformers import BertConfig
 
     config = {
-      'vocab_size':30522, 'hidden_size':1024, 'num_hidden_layers':24, 'num_attention_heads':16, 
-      'intermediate_size':4096, 'hidden_dropout_prob':0.1, 'attention_probs_dropout_prob':0.1,
+      'vocab_size':30522, 'hidden_size':4, 'num_hidden_layers':4, 'num_attention_heads':4,
+      'intermediate_size':128, 'hidden_dropout_prob':0.1, 'attention_probs_dropout_prob':0.1,
       'max_position_embeddings':512, 'type_vocab_size':2
       }
 
